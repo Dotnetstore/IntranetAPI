5b5d6ec4bf6616ceda6f60c26d4d92bdeeb6ba12
Hans Sjödin <hasse29@hotmail.com>
2024-03-10 20:49:05 +0100

    Added tests for corporateId

---
+++ Tests/Domain.Tests/ValueObjects/CorporateIds/CorporateIDTests.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Enums;
+using Domain.ValueObjects.CorporateIds;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace Domain.Tests.ValueObjects.CorporateIds;
+
+public class CorporateIDTests
+{
+    [Theory]
+    [InlineData("556056-6258")]
+    public void Should_return_correct_object(string id)
+    {
+        var corporateId = new CorporateId(id);
+
+        using (new AssertionScope())
+        {
+            corporateId.Should().BeOfType<CorporateId>();
+            corporateId.Id.Should().Be(id);
+        }
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Create_should_return_null(string value)
+    {
+        var result = CorporateId.Create(value);
+
+        result.Should().BeNull();
+    }
+
+    [Theory]
+    [InlineData("556056-6258")]
+    public void Create_should_return_valid_object(string value)
+    {
+        var result = CorporateId.Create(value);
+
+        using (new AssertionScope())
+        {
+            result!.Value.IsError.Should().BeFalse();
+            result!.Value.Value.Should().NotBeNull();
+        }
+    }
+
+    [Theory]
+    [InlineData("556056-6259")]
+    public void Create_should_return_validation_error(string value)
+    {
+        var result = CorporateId.Create(value);
+
+        using (new AssertionScope())
+        {
+            result!.Value.IsError.Should().BeTrue();
+            result!.Value.FirstError.Type.Should().Be(ErrorType.Validation);
+        }
+    }
+}
--- src/Domain/ValueObjects/CorporateIds/CorporateId.cs
+++ src/Domain/ValueObjects/CorporateIds/CorporateId.cs
@@ -3,11 +3,11 @@ using Domain.Services;
 
 namespace Domain.ValueObjects.CorporateIds;
 
-public record struct CorporateID(string? Id)
+public record struct CorporateId(string? Id)
 {
     public string? Id { get; init; } = Id;
 
-    public static Result<CorporateID>? Create(string? id)
+    public static Result<CorporateId>? Create(string? id)
     {
         if (string.IsNullOrWhiteSpace(id))
             return null;
@@ -15,7 +15,7 @@ public record struct CorporateID(string? Id)
         if (SwedishCorporateId.Valid(id))
         {
             var swedishCorporateId = new SwedishCorporateId(id);
-            return new CorporateID(swedishCorporateId.VatNumber);
+            return new CorporateId(swedishCorporateId.VatNumber);
         }
         
         return Error.Validation("CorporateID", "Invalid corporate id");
